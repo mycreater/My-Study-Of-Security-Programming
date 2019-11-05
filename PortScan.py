92b70beb9e813552523eb415ddc5830068b4ad72
mi <51748038+mi9898989@users.noreply.github.com>
2019-11-05 05:52:52 +0200

    Add files via upload

---
+++ PortScan.py
@@ -0,0 +1,11 @@
+import socket, sys
+
+ip = sys.argv[1]
+ports = range(1, 10000)
+
+for port in ports :
+    sock = socket.socket()
+    ret = sock.connect_ex((ip, port))
+
+    if ret == 0 :
+        print(str(port))
